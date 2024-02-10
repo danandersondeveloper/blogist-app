67fdd498a36dbc81c924b5ef8cb00d1aa3c2e86a
danandersondeveloper <dananderson.developer@gmail.com>
2024-02-10 19:49:13 +0200

    Update the error 404 page

--- blog-client/src/Pages/Error.js
+++ blog-client/src/Pages/Error.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"; 
+
 
 function Error() {
 	return(
 		<main className="content-wrapper">
 			<div class="row">
 				<h1>Error 404</h1>
+				<p>Not all those who wander are lost. However, this page does not exist. <Link to="/">Take me home!</Link></p>
 			</div>
 		</main>
 	);

18e11b8cd7fbbf2b8414c08f34c43cd3aa5c6047
danandersondeveloper <dananderson.developer@gmail.com>
2024-02-16 07:17:27 +0200

    Add new user route /users/search

--- blog-server/src/Routes/UserRoutes.js
+++ blog-server/src/Routes/UserRoutes.js
@@ -3,21 +3,43 @@ const router = express.Router();
 
 const userController = require("../Controllers/UserController");
 
+
+// @route /users
+
 router.get('/', (req, res) => {
 	userController.getUsers(req, res);
 });
+
+
+// @route /users/id
 
 router.get('/:id', (req, res) => {
 	userController.getUser(req, res);
 });
 
+
+// @route /users/search
+
+router.get('search', (req, res) => {
+	userController.searchUsers(req, res);
+});
+
+
+// @route /users/regster
+
 router.post('/register', (req, res) => {
 	userController.createUser(req, res);
 });
 
+
+// @route /users/edit/:id
+
 router.patch('/edit/:id', (req, res) => {
 	userController.editUser(req, res);
 });
+
+
+// @route /users/delete
 
 router.delete('/delete', (req, res) => {
 	userController.deleteUser(req, res);

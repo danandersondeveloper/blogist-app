d4fc5a9ca047e1b5515f65b84a1ff07728863aff
danandersondeveloper <dananderson.developer@gmail.com>
2024-02-16 16:38:50 +0200

    Work on create user

--- blog-client/src/Admin/Pages/Users/CreateUser.js
+++ blog-client/src/Admin/Pages/Users/CreateUser.js
@@ -1,7 +1,96 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function CreateUser() {
+
+	const navigate = useNavigate();
+
+	const [ userFirstName, setUserFirstName ] = useState(String);
+	const [ userLastName, setUserLastName ] = useState(String);
+	const [ userEmail, setUserEmail ] = useState(String);
+	const [ userPassword, setUserPassword ] = useState(String);
+	const [ userRole, setUserRole ] = useState(String);
+	const [ userIsActive, setUserIsActive ] = useState(Boolean);
+
+	const handleSubmit = (event) => {
+		
+		event.preventDefault();
+
+		//axios.post()
+
+	}
+
 	return(
-		<h1>Create New User</h1>
+		<main className="content-wrapper dashboard create-user">
+			<div className="row">
+				<div className="title">
+					<h1>Create New User</h1>
+					<button type="button" className="btn btn-dash-back" onClick={() => {navigate(-1)}}>Back</button>
+				</div>
+			</div>
+
+			<form onSubmit={ (event) => { handleSubmit(event) } }>
+				<div className="row">
+					<label htmlFor="first-name">First Name:</label>
+					<input
+						type="text"
+						name="first-name"
+						value={ userFirstName }
+						onChange={(event) => { setUserFirstName(event.target.value)} } 
+					/>
+				</div>
+				<div className="row">
+					<label htmlFor="last-name">Last Name:</label>
+					<input
+						type="text"
+						name="last-name"
+						value={ userLastName }
+						onChange={(event) => { setUserLastName(event.target.value) }} 
+					/>
+				</div>
+				<div className="row">
+					<label htmlFor="email">Email Address:</label>
+					<input
+						type="email"
+						name="email"
+						value={ userEmail }
+						onChange={(event) => { setUserEmail(event.target.value) }}
+					/>
+				</div>
+				<div className="row">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						value={ userPassword }
+						onChange={(event) => { setUserPassword(event.target.value) }}
+					/>
+				</div>
+				<div className="row">
+					<label htmlFor="user-role">User Role:</label>
+					<input
+						type="text"
+						name="user-role"
+						value={ userRole }
+						onChange={(event) => { setUserRole(event.target.value) }}
+					/>
+				</div>
+				<div className="row">
+					<label htmlFor="active">Active:</label>
+					<input
+						name="active"
+						type="checkbox"
+						checked={ userIsActive ? "checked" : "" }
+						onChange={ (event) => { setUserIsActive(!userIsActive) } }
+					/>
+				</div>
+				<div className="row">
+					<button type="submit">Save</button>
+				</div>
+			</form>
+
+		</main>
 	);
 }
 

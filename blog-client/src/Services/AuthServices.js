65f6d107546e539392a4e0081f3eac2b656486f8
danandersondeveloper <dananderson.developer@gmail.com>
2024-02-21 17:11:33 +0200

    Make AuthServices use .env variable for api requests

--- blog-client/src/Services/AuthServices.js
+++ blog-client/src/Services/AuthServices.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
+const BASE_URI = process.env.REACT_APP_API;
 
 // Authenticate a user
 
 const auth = async () => {
 	try {
 
-		const response = await axios.get('http://localhost:9000/auth', { withCredentials: true })
+		const response = await axios.get(`${BASE_URI}/auth`, { withCredentials: true })
 		return response.data;
 
 	} catch(error) {
@@ -20,7 +21,7 @@ const auth = async () => {
 const login = async (requestBody) =>  {
 	try {
 
-		const response =  await axios.post("http://localhost:9000/auth/login", requestBody, {withCredentials: true})
+		const response =  await axios.post(`${BASE_URI}/auth/login`, requestBody, {withCredentials: true})
 		return response.data;
 
 	} catch(error) {
@@ -36,7 +37,7 @@ const login = async (requestBody) =>  {
 const logout = async () => {
 	try {
 
-		const response = await axios.get('http://localhost:9000/logout', { withCredentials: true })
+		const response = await axios.get(`${BASE_URI}/logout`, { withCredentials: true })
 		return response.data.message;
 
 	} catch(error) {

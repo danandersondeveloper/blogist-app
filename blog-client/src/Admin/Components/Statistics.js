2503c35cde30c3a5b8aee5e8499904ac7b8653b9
danandersondeveloper <dananderson.developer@gmail.com>
2024-02-22 09:37:49 +0200

    Add stats into its own component file

---
+++ blog-client/src/Admin/Components/Statistics.js
@@ -0,0 +1,31 @@
+
+function Statistics() {
+	return(
+		<div className="basic-stats">
+			<div className="row">
+				<div className="col">
+					<div className="stat">
+						<div className="stat-title">
+							<span>Total Users:</span>
+						</div>
+						<div class="stat-number">
+							<span>45</span>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="stat">
+						<div className="stat-title">
+							<span>Total Blogs:</span>
+						</div>
+						<div class="stat-number">
+							<span>200</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Statistics;
--- blog-client/src/Admin/Dashboard.js
+++ blog-client/src/Admin/Dashboard.js
@@ -1,4 +1,7 @@
 
+// Components
+import Statistics from "./Components/Statistics";
+
 function Dashboard() {
 	return(
 		<main className="content-wrapper dashboard">
@@ -8,30 +11,7 @@ function Dashboard() {
 				</div>
 				<div className="content">
 					<div class="row">
-						<div className="basic-stats">
-							<div className="row">
-								<div className="col">
-									<div className="stat">
-										<div className="stat-title">
-											<span>Total Users:</span>
-										</div>
-										<div class="stat-number">
-											<span>45</span>
-										</div>
-									</div>
-								</div>
-								<div className="col">
-									<div className="stat">
-										<div className="stat-title">
-											<span>Total Blogs:</span>
-										</div>
-										<div class="stat-number">
-											<span>200</span>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+						<Statistics />
 					</div>
 				</div>
 			</div>

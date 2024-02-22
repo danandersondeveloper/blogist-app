7262279a5622c98115d8a5e2813f850c6348b70a
danandersondeveloper <dananderson.developer@gmail.com>
2024-02-22 09:27:03 +0200

    Start working on stats

--- blog-client/src/Admin/Dashboard.js
+++ blog-client/src/Admin/Dashboard.js
@@ -3,7 +3,37 @@ function Dashboard() {
 	return(
 		<main className="content-wrapper dashboard">
 			<div className="row">
-				<h1>Dashboard component</h1>
+				<div className="title">
+					<h1>Dashboard component</h1>
+				</div>
+				<div className="content">
+					<div class="row">
+						<div className="basic-stats">
+							<div className="row">
+								<div className="col">
+									<div className="stat">
+										<div className="stat-title">
+											<span>Total Users:</span>
+										</div>
+										<div class="stat-number">
+											<span>45</span>
+										</div>
+									</div>
+								</div>
+								<div className="col">
+									<div className="stat">
+										<div className="stat-title">
+											<span>Total Blogs:</span>
+										</div>
+										<div class="stat-number">
+											<span>200</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</main>
 	)

5054658de49dcbf9b26c4616511c3124a64598ad
danandersondeveloper <dananderson.developer@gmail.com>
2024-03-02 08:01:28 +0200

    Define admin routes

--- blog-server/src/Admin/Routes/UserRoutes.js
+++ blog-server/src/Admin/Routes/UserRoutes.js
@@ -1,6 +1,56 @@
 const express = require("express");
 const router = express.Router();
 
-// Backend User Routes
+const userController = require("../Controllers/UserController");
+
+
+//@route /admin/user/delete
+
+router.delete("/delete", (req, res) => {
+	
+});
+
+
+//@route /admin/user/update/:id
+
+router.patch("/update/:id", (req, res) => {
+
+});
+
+
+//@route /admin/user/create
+
+router.post("/create", (req, res) => {
+
+});
+
+
+//@route /admin/user/export
+
+router.post("/export", (req, res) => {
+
+});
+
+
+//@route /admin/user/search
+
+router.get("/search", (req, res) => {
+
+});
+
+
+//@route /admin/user/:id
+
+router.get("/:id", (req, res) => {
+
+});
+
+
+//@route /admin/user
+
+router.get("/", (req, res) => {
+
+});
+
 
 module.exports = router;

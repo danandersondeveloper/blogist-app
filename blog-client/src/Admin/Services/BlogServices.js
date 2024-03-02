dbae2fe747c9d176ed8c1bc8ce93333560e9b868
danandersondeveloper <dananderson.developer@gmail.com>
2024-03-02 09:28:14 +0200

    Update axios call to use private route

--- blog-client/src/Admin/Services/BlogServices.js
+++ blog-client/src/Admin/Services/BlogServices.js
@@ -4,7 +4,7 @@ const BASE_URI = process.env.REACT_APP_API;
 
 const getBlogs = async () => {
 	try {
-		return await axios.get( `${BASE_URI}/blog` );
+		return await axios.get( `${BASE_URI}/admin/blog` );
 	} catch (err) {
 		return err;
 	}

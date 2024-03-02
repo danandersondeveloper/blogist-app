0dcdefa13cb3563478cf475addf80fe8bbcd8533
danandersondeveloper <dananderson.developer@gmail.com>
2024-03-02 18:57:16 +0200

    Add blog routes

--- blog-server/src/Routes/BlogRoutes.js
+++ blog-server/src/Routes/BlogRoutes.js
@@ -4,8 +4,12 @@ const router = express.Router()
 // Controller imports
 const blogController = require("../Controllers/BlogController");
 
+router.get("/:id", (req, res) => {
+	blogController.getBlog(req, res);
+});
+
 router.get("/", (req, res) => {
-	blogController.getBlogs(req, res)
+	blogController.getBlogs(req, res);
 });
 
 module.exports = router;

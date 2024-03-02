568a687a59f94d5eaeee63a572f6455d562fc2b2
danandersondeveloper <dananderson.developer@gmail.com>
2024-03-02 09:27:40 +0200

    Setup admin / private routes

--- blog-server/src/Admin/Routes/BlogRoutes.js
+++ blog-server/src/Admin/Routes/BlogRoutes.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-// Backend Blog Routes
+const blogController = require("../Controllers/BlogController");
+
+router.get("/", (req, res) => {
+	blogController.getBlogs(req, res)
+});
+
+router.post( "/create", (req, res) => {
+	blogController.createBlog(req, res); 
+});
+
+router.patch("/update/:id", (req, res) => { 
+	res.json({message: "Request made to update / edit a blog"})
+});
+
+router.delete("/delete/:id", (req, res) => {
+	res.json({ message: "Request made to delete a specific blog" })
+});
 
 module.exports = router;
--- blog-server/src/Routes/BlogRoutes.js
+++ blog-server/src/Routes/BlogRoutes.js
@@ -4,12 +4,8 @@ const router = express.Router()
 // Controller imports
 const blogController = require("../Controllers/BlogController");
 
-router.get("/", (req, res) => { blogController.getBlogs(req, res) });
-
-router.post( "/create", (req, res) => { blogController.createBlog(req, res) } );
-
-router.patch("/edit/:id", (req, res) => { res.json({message: "Request made to update / edit a blog"}) });
-
-router.delete("/delete/:id", (req, res) => {res.json({ message: "Request made to delete a specific blog" })})
+router.get("/", (req, res) => {
+	blogController.getBlogs(req, res)
+});
 
 module.exports = router;

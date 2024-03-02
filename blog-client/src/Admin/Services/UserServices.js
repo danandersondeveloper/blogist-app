752c8c75c045f3998ae3979715bb59b7d56cde9d
danandersondeveloper <dananderson.developer@gmail.com>
2024-03-02 08:48:03 +0200

    Update API server calls to go to admin routes

--- blog-client/src/Admin/Services/UserServices.js
+++ blog-client/src/Admin/Services/UserServices.js
@@ -4,7 +4,7 @@ const BASE_URI = process.env.REACT_APP_API;
 
 const exportUsers = async () => {
 	try {
-		return axios.get(`${BASE_URI}/user/export`)	
+		return axios.get(`${BASE_URI}/admin/user/export`)	
 	} catch (error) {
 		return error
 	}
@@ -12,7 +12,7 @@ const exportUsers = async () => {
 
 const deleteUser = async ( requestBody ) => {
 	try {
-		return await axios.delete( `${BASE_URI}/user/delete/`, { data: { requestBody } } )
+		return await axios.delete( `${BASE_URI}/admin/user/delete/`, { data: { requestBody } } )
 	} catch (error) {
 		return error;
 	}
@@ -20,7 +20,7 @@ const deleteUser = async ( requestBody ) => {
 
 const updateUser = async ( userId, requestBody ) => {
 	try {
-		return await axios.patch( `${BASE_URI}/user/edit/${ userId }`, requestBody );
+		return await axios.patch( `${BASE_URI}/admin/user/update/${ userId }`, requestBody );
 	} catch(error) {
 		return error;
 	}
@@ -28,7 +28,7 @@ const updateUser = async ( userId, requestBody ) => {
 
 const createUser = async ( requestBody ) => {
 	try {
-		return await axios.post( `${BASE_URI}/user/register`, requestBody );
+		return await axios.post( `${BASE_URI}/admin/user/register`, requestBody );
 	} catch(error) {
 		return error;
 	}
@@ -36,7 +36,7 @@ const createUser = async ( requestBody ) => {
 
 const searchUsers = async ( searchString ) => {
 	try {
-		return await axios.get(`${BASE_URI}/user/search`, { params: { "search": searchString } })
+		return await axios.get(`${BASE_URI}/admin/user/search`, { params: { "search": searchString } })
 	} catch(error) {
 		return error
 	}
@@ -44,7 +44,7 @@ const searchUsers = async ( searchString ) => {
 
 const getUsers = async () => {
 	try {
-		return await axios.get( `${BASE_URI}/user/` );
+		return await axios.get( `${BASE_URI}/admin/user/` );
 	} catch(error) {
 		return error;
 	}

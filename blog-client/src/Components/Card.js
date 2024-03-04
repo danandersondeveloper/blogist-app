bbcf5515239d217e456afe90727f12ea57a08b80
danandersondeveloper <dananderson.developer@gmail.com>
2024-03-04 09:33:08 +0200

    Implement Card component for blogs

---
+++ blog-client/src/Components/Card.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+
+
+function Card(props) {
+	return(
+		<div className="card" key={ props.config.blogId }>
+			<Link to={`/blog/${ props.config.blogId }`}>
+				<div className="picture">
+					<img src={ props.config.blogPicture } alt="Blog picture" />
+				</div>
+				<div className="content">
+					<h4>{ props.config.blogTitle }</h4>
+					<p>{ props.config.shortDescription }</p>
+					<div className="extra-content">
+						<div className="buttons-wrapper">
+							<Link className="btn btn-primary small" to={`/blog/${ props.config.blogId }`}>Read blog</Link>
+							<Link to="#" className="btn btn-default small read-later">
+								Read later
+								<FontAwesomeIcon icon={ faFolderOpen } />
+							</Link>
+						</div>
+						<div className="created">
+							<span>Published: </span>
+							<span>{ props.config.blogCreated }</span>
+						</div>	
+					</div>
+				</div>
+			</Link>
+		</div>
+	)
+}
+
+export default Card;
--- blog-client/src/Pages/Home.js
+++ blog-client/src/Pages/Home.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
 
+// Services
 import { getBlogs } from "../Services/BlogService";
+
+// Components
+import Card from "../Components/Card";
 
 function Home() {
 
@@ -24,30 +28,15 @@ function Home() {
 				<div className="card-wrapper">
 					{
 						blogs.map(blog => (
-							<div className="card" key={ blog._id }>
-								<Link to={`/blog/${ blog._id }`}>
-									<div className="picture">
-										<img src={ blog.picture } alt="Blog picture" />
-									</div>
-									<div className="content">
-										<h4>{ blog.title }</h4>
-										<p>{ blog.shortDescription }</p>
-										<div className="extra-content">
-											<div className="buttons-wrapper">
-												<Link className="btn btn-primary small" to={`/blog/${ blog._id }`}>Read blog</Link>
-												<Link to="#" className="btn btn-default small read-later">
-													Read later
-													<FontAwesomeIcon icon={ faFolderOpen } />
-												</Link>
-											</div>
-											<div className="created">
-												<span>Published: </span>
-												<span>{ blog.created }</span>
-											</div>	
-										</div>
-									</div>
-								</Link>
-							</div>
+
+							<Card config={{
+								blogId: blog._id,
+								blogPicture: blog.picture,
+								blogTitle: blog.title,
+								blogShortDescription: blog.ShortDescription,
+								blogCreated: blog.created
+							}} />
+
 						))
 					}
 				</div>
